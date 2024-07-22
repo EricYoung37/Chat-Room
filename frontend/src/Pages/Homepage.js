fc13a46e2fdeb95186c2c5da3cea3272b271ccd9
Eric Y <yang.5490@buckeyemail.osu.edu>
2024-07-21 20:13:15 -0400

    login & signup ui

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Center,
+  Container,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Tab,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const Homepage = () => {
-  return <div>Home</div>;
-}
+  return (
+    <Container maxW="xl" centerContent>
+      <Center
+        d="flex"
+        p={2}
+        bg="white"
+        w="80%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="xl" fontFamily="Playwrite AU QLD">
+          Chat Room
+        </Text>
+      </Center>
 
-export default Homepage
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Homepage;
