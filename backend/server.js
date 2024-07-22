6d8347f869240c2b115f10bf22ab430eea91ccc1
Eric Y <yang.5490@buckeyemail.osu.edu>
2024-07-21 20:41:27 -0400

    connected to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./test-data/chats");
+const connectDB = require("./config/db");
 
-const app = express();
 dotenv.config();
+connectDB(); // must be preceded by dotenv
+const app = express();
 
 app.get("/", (req, res) => {
-    res.send("API is Running");
+  res.send("API is Running");
 });
 
-app.get("/api/chats", (req,res) => {
-    res.send(chats);
-})
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
 
-app.get("/api/chats/:id", (req,res) => {
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
 
 const PORT = process.env.PORT || 5000;
